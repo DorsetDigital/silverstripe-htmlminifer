ad5330fcc03ad267b33d0d94dad53f00a44ff9bf
Tim <DorsetDigital@users.noreply.github.com>
2026-09-18 17:01:48 +0200

    Update middleware for CMS 6

--- src/Middleware.php
+++ src/Middleware.php
@@ -5,100 +5,78 @@ namespace DorsetDigital\SSMinifier;
 use SilverStripe\Admin\AdminRootController;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Control\Middleware\HTTPMiddleware;
 use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Injector\Injectable;
-use SilverStripe\View\HTML;
 use voku\helper\HtmlMin;
 
 class Middleware implements HTTPMiddleware
 {
-
     use Injectable;
     use Configurable;
 
     /**
-     * @config
-     *
-     * Enable minification
-     * @var bool
+     * Enable minification.
      */
-    private static $enable = false;
-
+    private static bool $enable = false;
 
     /**
-     * @config
-     *
-     * Enable in dev mode
-     * @var bool
+     * Enable minification in dev mode.
      */
-    private static $enable_in_dev = false;
+    private static bool $enable_in_dev = false;
 
-
-    /**
-     * Process the request
-     * @param HTTPRequest $request
-     * @param $delegate
-     * @return
-     */
     public function process(HTTPRequest $request, callable $delegate)
     {
         $response = $delegate($request);
 
-        if (($this->canRun() === true) && ($response !== null)) {
-
-            if (($this->getIsAdmin($request) === false) && ($this->getIsSitemap($request) === false)) {
-                $body = $response->getBody();
-                $minifier = new HtmlMin();
-                $minBody = $minifier->minify($body);
-                $response->setBody($minBody);
-            }
-
+        if (!$response instanceof HTTPResponse || !$this->canRun()) {
+            return $response;
         }
+
+        if ($this->getIsAdmin($request) || $this->getIsSitemap($request) || !$this->getIsHtml($response)) {
+            return $response;
+        }
+
+        $body = $response->getBody();
+
+        if ($body === null || $body === '') {
+            return $response;
+        }
+
+        $response->setBody((new HtmlMin())->minify($body));
 
         return $response;
     }
 
-    /**
-     * Check if we're OK to execute
-     * @return bool
-     */
-    private function canRun()
+    private function canRun(): bool
     {
-        $confEnabled = $this->config()->get('enable');
-        $devEnabled = ((!Director::isDev()) || ($this->config()->get('enable_in_dev')));
-        return ($confEnabled && $devEnabled);
+        $config = $this->config();
+
+        return (bool) $config->get('enable')
+            && (!Director::isDev() || (bool) $config->get('enable_in_dev'));
     }
 
-
-    /**
-     * Determine whether the website is being viewed from an admin protected area or not
-     * (shamelessly based on https://github.com/silverstripe/silverstripe-subsites)
-     *
-     * @param HTTPRequest $request
-     * @return bool
-     */
-    private function getIsAdmin(HTTPRequest $request)
+    private function getIsAdmin(HTTPRequest $request): bool
     {
-        $adminPath = AdminRootController::admin_url();
-        $currentPath = rtrim($request->getURL(), '/') . '/';
-        if (substr($currentPath, 0, strlen($adminPath)) === $adminPath) {
-            return true;
-        }
-        return false;
+        $adminPath = trim(AdminRootController::admin_url(), '/');
+        $currentPath = trim($request->getURL(), '/');
+
+        return $currentPath === $adminPath
+            || str_starts_with($currentPath, $adminPath . '/');
     }
 
-    /**
-     * Checks if the current request is for a sitemap file
-     * @param HTTPRequest $request
-     * @return bool
-     */
-    private function getIsSitemap(HTTPRequest $request)
+    private function getIsSitemap(HTTPRequest $request): bool
     {
-        $currentPath = $request->getURL();
-        if (stristr($currentPath, 'sitemap.xml')) {
-            return true;
-        }
-        return false;
+        return str_contains(strtolower($request->getURL()), 'sitemap.xml');
+    }
+
+    private function getIsHtml(HTTPResponse $response): bool
+    {
+        $contentType = strtolower((string) $response->getHeader('content-type'));
+
+        return $contentType === ''
+            || str_contains($contentType, 'text/html')
+            || str_contains($contentType, 'application/xhtml+xml');
     }
 }
